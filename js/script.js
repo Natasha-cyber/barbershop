ea2400d6f0c01e5cd277216e436ece35abddbd55
Natalya <scheikina.nat@ya.ru>
2019-10-16 12:35:02 +0300

    ..

--- js/script.js
+++
@@ -1,6 +0,0 @@
-var link = document.querySelector(".login");
-var popup = document.querySelector(".modal_conent");
-link.addEventListener("click", function(event) {
-  event.preventDefault();
-  popup.classList.add("modal_content_show");
-});
