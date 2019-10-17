7e808de1038c73d25b2c646def499b0fd1a22f2e
Natalya <scheikina.nat@ya.ru>
2019-10-17 19:57:20 +0300

    Попробуем добавить оверлей

--- js/popup.js
+++ js/popup.js
@@ -5,10 +5,12 @@
   var login = popup.querySelector("[name=login]");
   var password = popup.querySelector("[name=password]");
   var storage = localStorage.getItem("login");
+  var overlay = document.querySelector(".modal_overlay");
 
   link.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.add("modal_content_show");
+    overlay.classList.add("overlay_show");
     if (storage) {
       login.value = storage;
       password.focus();
@@ -20,6 +22,7 @@
   close.addEventListener("click", function(event) {
     event.preventDefault();
     popup.classList.remove("modal_content_show");
+    overlay.classList.remove("overlay_show");
     popup.classList.remove("modal_error");
   });
 
@@ -34,8 +37,9 @@
 
   window.addEventListener("keydown", function(event) {
     if (event.keyCode === 27) {
-      if (popup.classList.contains("modal_content_show")) {
+      if (popup.classList.contains("modal_content_show") && overlay.classList.contains("overlay_show") {
         popup.classList.remove("modal_content_show");
+        overlay.classList.remove("overlay_show");
         popup.classList.remove("modal_error");
       }
     }
