64e44b3e7bd74d786143f3b90632e7215d1b0f86
Natalya <scheikina.nat@ya.ru>
2019-10-17 20:03:08 +0300

    Проект почти закончен!

--- js/map.js
+++ js/map.js
@@ -1,22 +1,26 @@
 var mapOpen = document.querySelector(".js_open_map");
 var mapPopup = document.querySelector(".modal_content_map");
 var mapClose = mapPopup.querySelector(".modal_content_close");
+var mapOverlay = document.querySelector(".modal_overlay");
 
 mapOpen.addEventListener("click", function(event) {
   event.preventDefault();
   mapPopup.classList.add("modal_content_show");
+  mapOverlay.classList.add("overlay_show");
 });
 
 mapClose.addEventListener("click", function(event) {
   event.preventDefault();
   mapPopup.classList.remove("modal_content_show");
+  mapOverlay.classList.remove("overlay_show");
 });
 
 
 window.addEventListener("keydown", function(event) {
   if (event.keyCode === 27) {
-    if (mapPopup.classList.contains("modal_content_show")) {
+    if (mapPopup.classList.contains("modal_content_show") && mapOverlay.classList.contains("overlay_show")) {
       mapPopup.classList.remove("modal_content_show");
+      mapOverlay.classList.remove("overlay_show");
     }
   }
 });
